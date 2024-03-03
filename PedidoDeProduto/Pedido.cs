3b240172c4e7e4774c98603b6c11663bf4948845
Marcelo Siqueira Oliveira <mss.siqueira@gmail.com>
2024-03-03 15:17:13 -0300

    segundo commit com O.O.

---
+++ PedidoDeProduto/Pedido.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Pedido
+{
+    class Pedido
+    {
+        public string NomeProduto;
+        public double QuantidadeProduto;
+
+        public Pedido(string nomeProduto, double quantidadeProduto)
+        {
+            NomeProduto = nomeProduto;
+            QuantidadeProduto = quantidadeProduto;
+        }
+
+        public double CalcularPrecoTotal()
+        {
+            double preco = 55.00;
+            double precoQuantidade = preco * QuantidadeProduto;
+
+            if (QuantidadeProduto <= 10)
+            {
+                return precoQuantidade;
+            }
+            else if (QuantidadeProduto > 10 && QuantidadeProduto < 21)
+            {
+                return precoQuantidade * 0.9;
+            }
+            else if (QuantidadeProduto >= 21 && QuantidadeProduto < 50)
+            {
+                return precoQuantidade * 0.8;
+            }
+            else
+            {
+                return precoQuantidade * 0.85;
+            }
+        }
+
+        public void ExibirDetalhesPedido()
+        {
+            double precoTotal = CalcularPrecoTotal();
+
+            Console.WriteLine($"O pedido de {QuantidadeProduto} {NomeProduto} ficou no total de {precoTotal:c2}");
+        }
+    }
+}
--- PedidoDeProduto/Program.cs
+++ PedidoDeProduto/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace PedidoDeProduto
+namespace Pedido
 {
     class Program
     {
@@ -12,26 +12,10 @@ namespace PedidoDeProduto
             Console.WriteLine("Qual a quantidade do produto:");
             double quantidadeProduto = double.Parse(Console.ReadLine());
 
-            double preco = 55.00;
-            double precoQuantidade = preco * quantidadeProduto;
+            Pedido pedido = new Pedido(nomeProduto, quantidadeProduto);
 
-            if (quantidadeProduto <= 10)
-            {
-                Console.WriteLine($"O pedido de {quantidadeProduto} {nomeProduto} ficou no total de {precoQuantidade:c2}");
-            }
-            else if (quantidadeProduto > 10 && quantidadeProduto < 21)
-            {
-                Console.WriteLine($"O pedido de {quantidadeProduto} {nomeProduto} ficou no total de {precoQuantidade * 0.9:c2}");
-            }
-            else if (quantidadeProduto >= 21 && quantidadeProduto < 50)
-            {
-                Console.WriteLine($"O pedido de {quantidadeProduto} {nomeProduto} ficou no total de {precoQuantidade * 0.8:c2}");
-            }
-            else
-            {
-                Console.WriteLine($"O pedido de {quantidadeProduto} {nomeProduto} ficou no total de {precoQuantidade * 0.85:c2}");
-            }
+            pedido.ExibirDetalhesPedido();
         }
     }
 }
-   
+
